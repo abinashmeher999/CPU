788f480b20b4a5eb0ae7871b829faa224fd50ce7
yvshri <yvshrivastava@gmail.com>
2015-11-13 02:02:02 +0530

    just changed the indentn

--- address_mux.v
+++ address_mux.v
@@ -10,15 +10,15 @@
 `include "constants.v"
 
 module address_mux(
-input[15:0] pc_value,
-input[15:0] mar_value,
-input[4:0] state,
-output[15:0] address_bus
+	input[15:0] pc_value,
+	input[15:0] mar_value,
+	input[4:0] state,
+	output[15:0] address_bus
 );
 
-	assign address_bus = (state === `S_FETCH_MEMORY 
-		|| state === `S_STORE_MEMORY 
-		|| state === `S_TEMP_FETCH 
+	assign address_bus = (state === `S_FETCH_MEMORY 	
+		|| state === `S_STORE_MEMORY  
+		|| state === `S_TEMP_FETCH  
 		|| state === `S_TEMP_STORE) 
 	? mar_value : pc_value;
 endmodule
